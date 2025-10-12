b7f2ef64ce7e22ee0f73b6fe1ec4220a04c902e3
René Lebherz <me@renelebherz.com>
2025-10-12 21:12:17 +0200

    chore(deps): Update dependency com.google.testing.compile:compile-testing to v0.23.0 (#24)
    * chore(deps): Update dependency com.google.testing.compile:compile-testing to v0.23.0
    
    * fix: test errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,9 +77,12 @@ tasks.withType(Javadoc::class.java).configureEach {
 tasks.test {
 	useJUnitPlatform()
 	jvmArgs(
-		"--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
-		"--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
-		"--add-exports=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED"
+        "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
+        "--add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED"
 	)
 }
 
